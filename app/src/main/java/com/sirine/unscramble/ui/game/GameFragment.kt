8224caeb2c6eaa0460877f9a2ef15de747bc4f79
sirinebensalah <sirinebensalah120@gmail.com>
2021-11-29 10:43:57 +0100

    store data in view model

---
+++ app/src/main/java/com/sirine/unscramble/ui/game/GameFragment.kt
@@ -0,0 +1,134 @@
+package com.sirine.unscramble.ui.game
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sirine.unscramble.R
+import com.sirine.unscramble.databinding.FragmentGameBinding
+import com.sirine.unscramble.ui.GameViewModel
+
+/**
+ * Fragment where the game is played, contains the game logic.
+ */
+class GameFragment : Fragment() {
+
+    private var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
+
+    private val viewModel: GameViewModel by viewModels()
+
+    // Binding object instance with access to the views in the game_fragment.xml layout
+    private lateinit var binding: FragmentGameBinding
+
+    // Create a ViewModel the first time the fragment is created.
+    // If the fragment is re-created, it receives the same GameViewModel instance created by the
+    // first fragment
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout XML file and return a binding object instance
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        Log.d("GameFragment", "GameFragment created/re-created!")
+        Log.d("GameFragment", "Word: ${viewModel.currentScrambledWord} " +
+                "Score: ${viewModel.score} WordCount: ${viewModel.currentWordCount}")
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Setup a click listener for the Submit and Skip buttons.
+        binding.submit.setOnClickListener { onSubmitWord() }
+        binding.skip.setOnClickListener { onSkipWord() }
+        // Update the UI
+        updateNextWordOnScreen()
+        binding.score.text = getString(R.string.score, 0)
+        binding.wordCount.text = getString(
+            R.string.word_count, 0, MAX_NO_OF_WORDS
+        )
+    }
+
+    /*
+    * Checks the user's word, and updates the score accordingly.
+    * Displays the next scrambled word.
+    */
+    private fun onSubmitWord() {
+        currentScrambledWord = getNextScrambledWord()
+        currentWordCount++
+        score += SCORE_INCREASE
+        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
+        binding.score.text = getString(R.string.score, score)
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    /*
+     * Skips the current word without changing the score.
+     * Increases the word count.
+     */
+    private fun onSkipWord() {
+        currentScrambledWord = getNextScrambledWord()
+        currentWordCount++
+        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    /*
+     * Gets a random word for the list of words and shuffles the letters in it.
+     */
+    private fun getNextScrambledWord(): String {
+        val tempWord = allWordsList.random().toCharArray()
+        tempWord.shuffle()
+        return String(tempWord)
+    }
+
+    /*
+     * Re-initializes the data in the ViewModel and updates the views with the new data, to
+     * restart the game.
+     */
+    private fun restartGame() {
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    /*
+     * Exits the game.
+     */
+    private fun exitGame() {
+        activity?.finish()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("GameFragment", "GameFragment destroyed!")
+    }
+
+
+
+    /*
+    * Sets and resets the text field error status.
+    */
+    private fun setErrorTextField(error: Boolean) {
+        if (error) {
+            binding.textField.isErrorEnabled = true
+            binding.textField.error = getString(R.string.try_again)
+        } else {
+            binding.textField.isErrorEnabled = false
+            binding.textInputEditText.text = null
+        }
+    }
+
+    /*
+     * Displays the next scrambled word on screen.
+     */
+    private fun updateNextWordOnScreen() {
+        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+    }
+}
